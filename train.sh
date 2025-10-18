29e2d1caa6eb7fd5b3461aadc5f58f8d07984a7d
JielongHe <jielonghe9@gmail.com>
2025-10-19 01:15:55 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=10
 #SBATCH --gpus=4
 #SBATCH --partition=gpu_h100
-#SBATCH --time=36:00:00
+#SBATCH --time=32:00:00
 #SBATCH --mem=84G
 #SBATCH --exclusive
-#SBATCH --job-name=qwen
-#SBATCH -o ./log/h100_test.out
+#SBATCH --job-name=qwen1
+#SBATCH -o ./log/qwen_%j.out
+
+# =============== 生成时间戳 ===============
+TIMESTAMP=$(date +"%Y%m%d_%H%M")
+LOG_DIR="./log"
+OUTPUT_DIR="./check_qwen3/checkpoints_${TIMESTAMP}"
+
+
 
 # =============== 加载 Snellius 2023 工具链 + CUDA ===============
 module load 2023
 module load CUDA/12.4.0
 
 nvidia-smi
-
 
 if [ -z "$CUDA_HOME" ]; then
     echo "❌ CUDA_HOME not set after module load!"
@@ -47,10 +53,8 @@ if [ $? -ne 0 ]; then
 fi
 
 # =============== 启动训练 ===============
-# ... 前面环境加载省略 ...
-
 MASTER_ADDR="127.0.0.1"
-MASTER_PORT=$(shuf -i 20000-29999 -n 1)   # ✅ 正确：没有反斜杠
+MASTER_PORT=$(shuf -i 20000-29999 -n 1)
 NPROC_PER_NODE=4
 
 torchrun \
@@ -58,15 +62,15 @@ torchrun \
     --master_addr=$MASTER_ADDR \
     --master_port=$MASTER_PORT \
     qwenvl/train/train_qwen.py \
-    --model_name_or_path "Qwen/Qwen2.5-VL-3B-Instruct" \
+    --model_name_or_path "./check_qwen3/checkpoints_20251016_1639" \
     --tune_mm_llm True \
     --tune_mm_vision False \
     --tune_mm_mlp False \
     --dataset_use "samm_data" \
-    --output_dir "./checkpoints_10_8" \
+    --output_dir "$OUTPUT_DIR" \
     --cache_dir "./cache" \
     --bf16 \
-    --per_device_train_batch_size 6 \
+    --per_device_train_batch_size 12 \
     --gradient_accumulation_steps 4 \
     --learning_rate 2e-7 \
     --mm_projector_lr 1e-5 \
@@ -77,7 +81,7 @@ torchrun \
     --data_packing True \
     --max_pixels $((576*28*28)) \
     --min_pixels $((16*28*28)) \
-    --num_train_epochs 6 \
+    --num_train_epochs 12 \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --weight_decay 0.01 \
